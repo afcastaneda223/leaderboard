78e7fefaab945857f8f90ffe2808df93966b23f3
afcastaneda223 <af.castaneda223@gmail.com>
2021-07-27 18:37:13 -0300

    add webpack files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+entry: './src/index.js',
+output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+},
+plugins: [
+    new HtmlWebpackPlugin({
+    template: './src/index.html'
+    }),
+],
+devServer: {
+    contentBase: './dist',
+},
+module: {
+    rules: [
+    {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+    },
+    ],
+},
+};
